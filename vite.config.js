7b3fd48e26661cd35a0298e385828efc25feaf88
Jared Addison <77221122+jaredaddison@users.noreply.github.com>
2021-06-14 01:51:51 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path, { join } from "path"
 import ViteComponents from 'vite-plugin-components'
+import ViteIcons from 'vite-plugin-icons'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-     ViteComponents()
-     ],
+  plugins: [vue(),ViteComponents(),ViteIcons()],
   resolve: {
     alias: {
-      "~":path.resolve(__dirname,"/src/components"),
-      "µ":path.resolve(__dirname,"/src/views"),
+      "@":path.resolve(__dirname,"/src/components"),
+      "~":path.resolve(__dirname,"/src/views"),
     },
   },
   server: {
