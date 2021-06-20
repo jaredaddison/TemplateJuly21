1b2d569c3a134f4f74413d7a362018fcfc4b1eeb
Jared <drjaredaddison@gmail.com>
2021-06-20 17:04:42 +0800

    ni
    jij

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,17 @@
 // @ts-nocheck
-import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
-
+import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: Home,
         meta: {title:'Home Dashboard'}
-    },
-    {
-        path: '/pricing',
-        name: 'pricing',
-        component: Pricing,
-        meta: { title: 'Pricing' }
-    },
+    }
 ]
 
-const router = createRouter( {
+const router = createRouter({
     history: createWebHistory(),
     routes,
-} )
-
+})
 export default router
